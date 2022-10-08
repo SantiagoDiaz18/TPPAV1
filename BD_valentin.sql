ab3ad65fe1002d7ee3932f71649d775abf285866
Valentin Arriaga <80863532+ValenArriaga@users.noreply.github.com>
2022-10-08 17:50:18 -0300

    Add files via upload

--- BD_valentin.sql
+++ BD_valentin.sql
@@ -1,6 +1,6 @@
 USE [master]
 GO
-/****** Object:  Database [BDPav1TP]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Database [BDPav1TP]    Script Date: 8/10/2022 17:48:28 ******/
 CREATE DATABASE [BDPav1TP]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -80,7 +80,7 @@ ALTER DATABASE [BDPav1TP] SET QUERY_STORE = OFF
 GO
 USE [BDPav1TP]
 GO
-/****** Object:  Table [dbo].[Barrios]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[Barrios]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -96,7 +96,7 @@ CREATE TABLE [dbo].[Barrios](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Clientes]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[Clientes]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -117,7 +117,7 @@ CREATE TABLE [dbo].[Clientes](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[DetalleFactura]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[DetalleFactura]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -136,7 +136,7 @@ CREATE TABLE [dbo].[DetalleFactura](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Facturas]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[Facturas]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -156,7 +156,7 @@ CREATE TABLE [dbo].[Facturas](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[FormasPago]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[FormasPago]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -170,7 +170,7 @@ CREATE TABLE [dbo].[FormasPago](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Localidades]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[Localidades]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -186,7 +186,7 @@ CREATE TABLE [dbo].[Localidades](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ProductoColor]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[ProductoColor]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -200,7 +200,7 @@ CREATE TABLE [dbo].[ProductoColor](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ProductoMaterial]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[ProductoMaterial]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -214,7 +214,7 @@ CREATE TABLE [dbo].[ProductoMaterial](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Productos]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[Productos]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -239,7 +239,7 @@ CREATE TABLE [dbo].[Productos](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Proveedores]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[Proveedores]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -257,7 +257,7 @@ CREATE TABLE [dbo].[Proveedores](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[TipoDocumento]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[TipoDocumento]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -271,7 +271,7 @@ CREATE TABLE [dbo].[TipoDocumento](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[TipoProductos]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[TipoProductos]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -285,7 +285,7 @@ CREATE TABLE [dbo].[TipoProductos](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[TipoSexo]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[TipoSexo]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -299,7 +299,7 @@ CREATE TABLE [dbo].[TipoSexo](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Usuarios]    Script Date: 7/10/2022 10:59:29 ******/
+/****** Object:  Table [dbo].[Usuarios]    Script Date: 8/10/2022 17:48:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -315,6 +315,170 @@ CREATE TABLE [dbo].[Usuarios](
 	[idUsuario] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Barrios] ON 
+GO
+INSERT [dbo].[Barrios] ([idBarrio], [nombreBarrio], [idLocalidad], [borrado]) VALUES (1, N'Villa Allende', 1, 0)
+GO
+INSERT [dbo].[Barrios] ([idBarrio], [nombreBarrio], [idLocalidad], [borrado]) VALUES (2, N'Alta Cordoba', 2, 0)
+GO
+INSERT [dbo].[Barrios] ([idBarrio], [nombreBarrio], [idLocalidad], [borrado]) VALUES (3, N'Arguello', 3, 0)
+GO
+INSERT [dbo].[Barrios] ([idBarrio], [nombreBarrio], [idLocalidad], [borrado]) VALUES (4, N'Barrio22', 2, 0)
+GO
+SET IDENTITY_INSERT [dbo].[Barrios] OFF
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (2, 3, N'ffdsfsdfsdf', N'werasfdasfdsfsdf', CAST(N'2022-09-30' AS Date), 2, 2, 23235)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (23, 3, N'asdsdfsdfssdfsSDFddf', N'rsfdsf', CAST(N'2022-09-30' AS Date), 3, 2, 2312)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (24, 3, N'fasdfasf', N'sfdsdfsdfsdfsdf', CAST(N'2022-09-30' AS Date), 2, 2, 23432)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (234, 1, N'sfd', N'sdf', CAST(N'2022-09-30' AS Date), 1, 1, 325432)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (345, 3, N'a', N'sfd', CAST(N'2022-09-30' AS Date), 3, 1, 2342)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (23423, 2, N'sffd', N'sfd', CAST(N'2022-09-30' AS Date), 1, 1, 342)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (23432, 3, N'fsd', N'sdf', CAST(N'2022-09-30' AS Date), 2, 2, 23432)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (24324, 1, N'ads', N'asda', CAST(N'2022-09-30' AS Date), 2, 3, 123)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (324234, 3, N'sdfsdf', N'sdfs', CAST(N'2022-09-30' AS Date), 2, 2, 325423)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (325432, 1, N'sf', N'sf', CAST(N'2022-09-30' AS Date), 1, 1, 3242)
+GO
+INSERT [dbo].[Clientes] ([nroDoc], [tipoDoc], [apellido], [nombre], [fechaNacimiento], [idSexo], [idBarrio], [telefono]) VALUES (45482076, 2, N'Pary2dsffs', N'Ronald', CAST(N'2002-01-31' AS Date), 2, 1, 3808660)
+GO
+SET IDENTITY_INSERT [dbo].[Localidades] ON 
+GO
+INSERT [dbo].[Localidades] ([idLocalidad], [nombreLocalidad], [idDepartamento], [borrado]) VALUES (1, N'Cordoba', 1, 0)
+GO
+INSERT [dbo].[Localidades] ([idLocalidad], [nombreLocalidad], [idDepartamento], [borrado]) VALUES (2, N'Bouwer', 2, 0)
+GO
+INSERT [dbo].[Localidades] ([idLocalidad], [nombreLocalidad], [idDepartamento], [borrado]) VALUES (3, N'Jesus Maria', 3, 0)
+GO
+INSERT [dbo].[Localidades] ([idLocalidad], [nombreLocalidad], [idDepartamento], [borrado]) VALUES (4, N'Rio Ceballo', 4, 0)
+GO
+SET IDENTITY_INSERT [dbo].[Localidades] OFF
+GO
+SET IDENTITY_INSERT [dbo].[ProductoColor] ON 
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (1, N'Azul')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (2, N'Rojo')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (3, N'Verde')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (4, N'Marron')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (5, N'Naranja')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (6, N'Gris')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (7, N'Blanco')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (8, N'Rosa')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (9, N'Amarillo')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (10, N'Violeta')
+GO
+INSERT [dbo].[ProductoColor] ([idColor], [descripcion]) VALUES (11, N'Negro')
+GO
+SET IDENTITY_INSERT [dbo].[ProductoColor] OFF
+GO
+SET IDENTITY_INSERT [dbo].[ProductoMaterial] ON 
+GO
+INSERT [dbo].[ProductoMaterial] ([idMaterial], [descripcion]) VALUES (1, N'Madera')
+GO
+INSERT [dbo].[ProductoMaterial] ([idMaterial], [descripcion]) VALUES (2, N'Plastico')
+GO
+INSERT [dbo].[ProductoMaterial] ([idMaterial], [descripcion]) VALUES (3, N'Metal')
+GO
+INSERT [dbo].[ProductoMaterial] ([idMaterial], [descripcion]) VALUES (4, N'Ergonomica')
+GO
+SET IDENTITY_INSERT [dbo].[ProductoMaterial] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Productos] ON 
+GO
+INSERT [dbo].[Productos] ([idProducto], [precio], [costo], [idColor], [idMaterial], [peso], [largo], [ancho], [alto], [stock], [idProveedor], [idTipo], [periodoGarantia]) VALUES (15, 1000, 700, 6, 1, 234, 123, 543, 234, 123, 1, 5, CAST(N'2022-07-11T09:44:43.000' AS DateTime))
+GO
+SET IDENTITY_INSERT [dbo].[Productos] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Proveedores] ON 
+GO
+INSERT [dbo].[Proveedores] ([idProveedor], [razonSocial], [telefono], [nombre], [apellido], [idBarrio]) VALUES (1, N'wew', 234, N'Luis', N'Flores', 1)
+GO
+INSERT [dbo].[Proveedores] ([idProveedor], [razonSocial], [telefono], [nombre], [apellido], [idBarrio]) VALUES (2, N'ewr', 234, N'Juan', N'Contreras', 2)
+GO
+INSERT [dbo].[Proveedores] ([idProveedor], [razonSocial], [telefono], [nombre], [apellido], [idBarrio]) VALUES (3, N'empresa', 233, N'Lourdes', N'Barrios', 2)
+GO
+INSERT [dbo].[Proveedores] ([idProveedor], [razonSocial], [telefono], [nombre], [apellido], [idBarrio]) VALUES (5, N'asd', 23, N'asd', N'asdaasd', 1)
+GO
+SET IDENTITY_INSERT [dbo].[Proveedores] OFF
+GO
+SET IDENTITY_INSERT [dbo].[TipoDocumento] ON 
+GO
+INSERT [dbo].[TipoDocumento] ([idTipo], [descripcion]) VALUES (1, N'Dni')
+GO
+INSERT [dbo].[TipoDocumento] ([idTipo], [descripcion]) VALUES (2, N'Pasaporte')
+GO
+INSERT [dbo].[TipoDocumento] ([idTipo], [descripcion]) VALUES (3, N'Libreta')
+GO
+SET IDENTITY_INSERT [dbo].[TipoDocumento] OFF
+GO
+SET IDENTITY_INSERT [dbo].[TipoProductos] ON 
+GO
+INSERT [dbo].[TipoProductos] ([idTipo], [nombre]) VALUES (1, N'Silla')
+GO
+INSERT [dbo].[TipoProductos] ([idTipo], [nombre]) VALUES (2, N'Mesa')
+GO
+INSERT [dbo].[TipoProductos] ([idTipo], [nombre]) VALUES (3, N'Mesa Ratona')
+GO
+INSERT [dbo].[TipoProductos] ([idTipo], [nombre]) VALUES (4, N'Sofa')
+GO
+INSERT [dbo].[TipoProductos] ([idTipo], [nombre]) VALUES (5, N'Sillon')
+GO
+SET IDENTITY_INSERT [dbo].[TipoProductos] OFF
+GO
+SET IDENTITY_INSERT [dbo].[TipoSexo] ON 
+GO
+INSERT [dbo].[TipoSexo] ([idSexo], [descripcionS]) VALUES (1, N'Femenino')
+GO
+INSERT [dbo].[TipoSexo] ([idSexo], [descripcionS]) VALUES (2, N'Masculino')
+GO
+INSERT [dbo].[TipoSexo] ([idSexo], [descripcionS]) VALUES (3, N'Otro')
+GO
+SET IDENTITY_INSERT [dbo].[TipoSexo] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Usuarios] ON 
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (5, N'ronald', N'123ro', N'ronaldpary780@gmail.com', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (7, N'santiago ', N'123san', N'santiagodiaz1@gmail.com', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (8, N'martin1', N'123mar', N'martinbaudino@gmail.com', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (11, N'federico1', N'123fe', N'federicotorres@gmail.com', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (2017, N'asdsadasdDFGDFG', N'asdasd', N'dfsdfsf', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (2018, N'werfsf', N'sdfsdf', N'sdfsdf', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (2019, N'sdfsd', N'sdf', N'sdff', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (2020, N'sdfsdf', N'sdfsdf', N'sdfsdf', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (3012, N'fAS', N'sdf', N'sdf', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (3013, N'sdf', N'sdf', N'sdf', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (3014, N'SDFSDFSDF', N'ASD', N'ASDAS', 0)
+GO
+INSERT [dbo].[Usuarios] ([idUsuario], [usuario], [contraseña], [mail], [borrado]) VALUES (3015, N'valentin', N'123', N'arriagavalen.03@gmail.com', 0)
+GO
+SET IDENTITY_INSERT [dbo].[Usuarios] OFF
 GO
 ALTER TABLE [dbo].[Barrios]  WITH CHECK ADD  CONSTRAINT [FK_Barrios_Localidades] FOREIGN KEY([idLocalidad])
 REFERENCES [dbo].[Localidades] ([idLocalidad])
